f16c1f64dcec96fedf9495d7f677ddd3ce9aa10b
Andrew Wipf <andrew.wipf09@gmail.com>
2016-04-13 03:51:23 +0000

    Caused issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407211730) do
+ActiveRecord::Schema.define(version: 20160413035006) do
 
   create_table "contacts", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.text     "comments"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
 end
