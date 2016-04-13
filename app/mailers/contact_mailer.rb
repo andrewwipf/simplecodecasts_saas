e27dca2234aee692d189d82a5d6cf17a6e06a685
Andrew Wipf <andrew.wipf09@gmail.com>
2016-04-13 03:31:22 +0000

    fixed bug

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ActionMailer::Base
-    default to 'andrew.wipf09@gmail.com'
+    default to: 'andrew.wipf09@gmail.com'
     
     def contact_email(name, email, body)
         @name = name
         @email = email
         @body = body
         
-        mail(to: "andrew.wipf09@gmail.com", from: email, subject: "Contact Form Message")
+        mail(from: email, subject: "Contact Form Message")
     end
 end
