fc57f1302e46cc9eecfc6d2e1761d44016412331
Andrew Wipf <andrew.wipf09@gmail.com>
2016-05-24 03:52:40 +0000

    Added notice and styles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,4 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :stripe_card_token, :email, :password, :password_confirmation) }
     end
-
 end
